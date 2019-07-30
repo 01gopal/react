cdec9fb150bfbb9e411b331789cc39705972d6f8
sharmg11 <GopalKrishan.Sharma@anz.com>
2019-07-31 09:23:00 +1000

    added all the components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import Menu from './component/Menu';
+import data from './data/recipes';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+window.React = React
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(
+    <Menu recipes={data}
+        title="Delicious Recipes" />,
+    document.getElementById("root")
+)
