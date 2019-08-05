7243c5fbd3258af2e29179e9fced7e102ab12dbe
Gopal Sharma <gopalsharma@192-168-1-102.tpgi.com.au>
2019-08-06 06:08:41 +1000

    star rating with state

--- src/component/star/StarRating.jsx
+++ src/component/star/StarRating.jsx
@@ -6,7 +6,7 @@ class StarRating extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        starsSelected: props.starsSelected
+        starsSelected: props.starsSelected || 0
     };
     this.change = this.change.bind(this);
   }
